491ecd661a595c4afedb2693d0f726098ce88ecf
Paradeluxe <paradeluxe3726@gmail.com>
2024-06-14 17:04:37 +0800

    Update lmm automation script

--- lmm_rt.py
+++ lmm_rt.py
@@ -15,52 +15,76 @@ Matrix = importr("Matrix")
 
 if __name__ == "__main__":
 
-
-    exptype = "exp2"
-    dep_var = "rt"  # ifcorr rt
-
     # 读取CSV文件
-    data = pd.read_csv(r"C:\Users\Lenovo-PC\Desktop\linguistic rhythm\Data_Experiment.csv", encoding="utf-8")
+    data = pd.read_csv("Data_Experiment.csv", encoding="utf-8")
 
     # 在Python中进行数据子集选择
-    data = data[(data['exp_type'] == exptype) & (data['ifanimal'] == True)]
+    data = data[(data['exp_type'] == "exp2") & (data['ifanimal'] == True)]
 
-    if dep_var == "rt":
-        data = data[data['ifcorr'] == 1]
+    # if dep_var == "rt":
+    data = data[data['ifcorr'] == 1]
 
     data['rt'] = data['rt'] * 1000
 
     # 定义因子变量
-    data['Tpriming'] = -0.5 * (data['priming'] == "priming") + 0.5
-    data['Tsyl'] = -0.5 * (data['syl'] == 2) + 0.5
-    data['Texp_type'] = -0.5 * (data['exp_type'] == "exp1") + 0.5
+    data['Tpriming'] = -0.5 * (data['priming'] == "priming") + 0.5 * (data['priming'] != "priming")
+    data['Tsyl'] = -0.5 * (data['syl'] == 2) + 0.5 * (data['syl'] != 2)
+    # data['Texp_type'] = -0.5 * (data['exp_type'] == "exp1") + 0.5 * (data['exp_type'] != "exp1")
 
     # 将pandas DataFrame转换为R的data.frame
-    # r_data = pandas2ri.py2ri(data)
     with (ro.default_converter + pandas2ri.converter).context():
         r_data = ro.conversion.get_conversion().py2rpy(data)
 
     # Create Formula and do the For loop
 
-
+    # Construct
     # 定义R的公式
-    formula = Formula("rt ~ Tpriming * Tsyl + (1 | sub) + (1 | word)")
+    fixed_factor = ["Tpriming", "Tsyl"]
+    random_factor = ["sub", "word"]
 
-    # 使用lmer函数拟合模型
-    if dep_var == "rt":
+    fixed_model = " * ".join(fixed_factor)
+    random_model = " + ".join([f"(1+Tpriming*Tsyl|{rf})" for rf in random_factor])
+    formula = Formula(f"rt ~ {fixed_model} + {random_model}")
+
+
+    for formula in formulas:
+        print(f"Running FORMULA: {formula}")
+
+        # 使用lmer函数拟合模型
         model1 = lmerTest.lmer(formula, REML=True, data=r_data)
-        # print(model1)
-        anova_model1 = stats.anova(model1, type=3, ddf="Kenward-Roger")
-        print(anova_model1)
-        print(type(anova_model1))
+
         summary_model1 = Matrix.summary(model1)
         print(summary_model1)
-        # ro.r('anova(model1, type=3, ddf="Kenward-Roger")')
+        with (ro.default_converter + pandas2ri.converter).context():
+            summary_model1 = ro.conversion.get_conversion().rpy2py(summary_model1)
 
 
-    else:
-        # 这里需要根据你的实际模型进行调整
-        pass
+
+        isSingular = summary_model1["optinfo"]["conv"]['lme4']["messages"][0] == "boundary (singular) fit: see help('isSingular')"
+
+        isGoodModel = not isSingular
+
+        if isGoodModel:
+            break
+        else:
+            continue
+
+
+    # ('methTitle', 'objClass', 'devcomp', 'isLmer', 'useScale', 'logLik', 'family', 'link', 'ngrps', 'coefficients', 'sigma', 'vcov', 'varcor', 'AICtab', 'call', 'residuals', 'fitMsgs', 'optinfo', 'corrSet')
+    # ('optimizer', 'control', 'derivs', 'conv', 'feval', 'message', 'warnings', 'val')
+
+    anova_model1 = stats.anova(model1, type=3, ddf="Kenward-Roger")
+    # print(anova_model1.colnames)
+    # print(anova_model1.rownames)
+
+    # anova_model1 change format
+    with (ro.default_converter + pandas2ri.converter).context():
+        anova_model1 = ro.conversion.rpy2py(anova_model1)
+
+    print(anova_model1["Pr(>F)"])
+
+
+    # print(summary_model1)
 
     # 进行emmeans分析
     # emmeans_result = emmeans.emmeans(model1, pairwise ~ Tsyl)
