4f831c411a3a4d618c4008be163638e1355d99bc
Paradeluxe <paradeluxe3726@gmail.com>
2024-07-13 11:20:13 +0800

    Update readme information

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='ezlmm',
-    version='0.0.2',
+    version='0.1.0',
     packages=find_packages(),
     install_requires=['numpy==1.26.4', 'pandas==2.2.2', 'rpy2==3.5.16'],
     authors=[{"name": 'Paradeluxe', "email": "paradeluxe3726@gmail.com"}],
-    description='Use linear mixed model in Python, but still based on R',
+    description='Python interface of Linear Mixed Model analysis in R language (e.g., lmerTest, lme4)',
     license='MIT',
     keywords='Linear Mixed Model',
     url='https://github.com/Paradeluxe/ezLMM',  # 你的项目主页
